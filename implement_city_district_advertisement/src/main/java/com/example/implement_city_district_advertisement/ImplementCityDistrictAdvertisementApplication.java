b4e80db843da1f5c38a521da7543d79cfead2905
hossein javadi <hosseinj13@yahoo.com>
2024-12-12 14:23:25 +0330

    Create project and add dependencies to the pom.xml

---
+++ implement_city_district_advertisement/src/main/java/com/example/implement_city_district_advertisement/ImplementCityDistrictAdvertisementApplication.java
@@ -0,0 +1,13 @@
+package com.example.implement_city_district_advertisement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementCityDistrictAdvertisementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplementCityDistrictAdvertisementApplication.class, args);
+    }
+
+}
